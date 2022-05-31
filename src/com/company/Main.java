db69550701198bc71323cc1582005941e4710dd7
Stam <bolaris.minas@gmail.com>
2022-05-31 23:48:43 +0300

    create jList on Parking.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,24 @@ import java.awt.event.ActionEvent;
 import java.sql.*;
 
 
-public abstract class Main {
+public abstract class Main extends Parking {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static  void main(String[] args) throws SQLException, ClassNotFoundException {
 
 
         AppUI appUI = new AppUI();
+
+        //SwingUtilities.invokeLater(Parking::new);
+
+
+
+
+
+
+
+
+
+
 
 
     }
--- src/com/company/Parking.java
+++ src/com/company/Parking.java
@@ -4,95 +4,118 @@ package com.company;
 //h klassi auti einai wste o xristis meta to singup h to login na dialegei to parking poy epithimei gia tin kratisi thesis
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.text.html.ListView;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.sql.*;
 
 public class Parking {
-    ArrayList<Parking> parkinglist;
-    String parkingarray[] = new String[]{"Parking Name", "Parking Location , Reservation Price" ,"Subscription Price", "Reservation Duration", "Subscription Duration"};
-    DefaultTableModel dtm;
+    JFrame app = new JFrame();
+    JList<CreateParking> parkinglist = new JList();
+    DefaultListModel<CreateParking> model = new DefaultListModel<>();
 
-    private static JFrame app;
-    private static JPanel panel;
-    private static JLabel parkingNameLabel;
-    private static JTextField parkingNameText;
-    private static JLabel locationLabel;
-    private static  JTextField locationText;
-    private static JButton addButton;
-    private static JTable table;
+   JLabel parkingnamelabel = new JLabel();
+   JPanel panel = new JPanel();
+   JSplitPane splitpane = new JSplitPane();
+ private String name;
 
-    int row;
-    int col;
 
-   public Parking(){
-        initComponents();
-        parkinglist = new ArrayList<>();
-        dtm = new DefaultTableModel(parkingarray , 0);
-        this.setLocationRelativeTo(null);
+    Parking() {
 
 
 
+        parkinglist.setModel(model);
+        model.addElement(new CreateParking("PapadoParking" , new String("Karaiskaki 87")));
+        splitpane.setLeftComponent(new JScrollPane(parkinglist));
+        panel.add(parkingnamelabel);
+        splitpane.setRightComponent(panel);
+        app.setTitle("Parking Selection");
+        app.setSize(500, 500);
+
+        app.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        app.add(splitpane);
+        app.pack();
+        app.setLocationRelativeTo(null);
+        app.add(splitpane);
+        app.add(panel);
+        panel.setLayout(null);
 
 
-           JFrame app = new JFrame("SELECT PARKING");
-           app.setSize(500,500);
-           app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-           app.setLayout(null);
-           app.add(panel);
-           app.setLocationRelativeTo(null);
-
-           panel.add(app);
-
-           panel.setLayout(null);
-
-           parkingNameLabel = new JLabel("Name");
-           parkingNameLabel.setBounds(50,40,80,25);
-           panel.add(parkingNameLabel);
-
-           parkingNameText = new JTextField();
-           parkingNameText.setBounds(100,40,80,25);
-           panel.add(parkingNameText);
-
-        locationLabel = new JLabel("Location");
-         locationLabel.setBounds(50,70,80,25);
-           panel.add(locationLabel);
-
-           locationText = new JTextField();
-           locationText.setBounds(120,70,80,25);
-           panel.add(locationText);
-
-          addButton = new JButton("ADD");
-         addButton.setBounds(50,100,80,25);
-          //addButton.addActionListener(this);
-
-       table = new JTable(dtm);
-       table.setSize(500,300);
-       panel.add(table);
-       table.setVisible(true);
+        app.setVisible(true);
 
 
 
 
-       app.setVisible(true);
+    }
 
 
+    public class CreateParking{
+       String name;
+       String location;
+
+       //dimiourgoume enan constructor gia ta orismata wste na mporoume na prosthetoume times line:25
+        public CreateParking(String papadoParking, String s) {
+
+        }
+    }
+
+
+   public String getName(){
+     return name;
    }
-
-
-
-    private void initComponents() {
+    public void setName(String name) {
+        this.name = name;
     }
 
-    private void setLocationRelativeTo(Object o) {
+    public static void setLocation(String newLocation) {
+       String location = newLocation;
+
+    }
+    @Override
+    public String toString(){
+
+        return name;
+    }
+
+    public Parking parking;
+
+    private Parking selectParkingFromDatabase(String parkin_id, String parking_name, String address, int num_of_spaces) {
+
+        Parking parking = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            final String url = "jdbc:mysql://localhost:3306/parkhubcity?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            final String USERNAME = "root";
+            final String PASSWORD = "";
+            Connection con = DriverManager.getConnection(url, USERNAME, PASSWORD);
+            if (con != null) {
+                System.out.println("Succesfull");
+
+            }
+        }
+        Statement stm = con.createStatement();
+        String sql = "SELECT * FROM  parking ";
+        ResultSet resultSet = stm.executeQuery("SELECT * FROM parking");
+        while(resultSet.next()){
+
+            System.out.println("parking_id : " +  resultSet.getString(1) + " " + "parking_name: " +resultSet.getString(2) + "" + "address :"+ resultSet.getString(3) + "name_of_spaces: " +resultSet.getInt(4));
+
+
+
+        }
+
+
+
+        stm.close();
+        con.close();
+
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
     }
 
 
-
-
-
+        return parking;
+}
 
 }
